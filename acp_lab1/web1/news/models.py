6f519edaddcba4322fdb405e9283eded2190cc92
guboyzasa <rachata147@gmail.com>
2021-12-15 14:32:07 +0700

    update 15-12-21

--- acp_lab1/web1/news/models.py
+++ acp_lab1/web1/news/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class newsPost(models.Model):
     title = models.CharField(max_length=150)
     newsContent = models.TextField()
-    
+    def __str__(self):
+        return self.title
--- acp_lab1/web1/news/views.py
+++ acp_lab1/web1/news/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import newsPost
 # Create your views here.
+
+def index(request):
+    newsposts = newsPost.objects.all()
+    return render(request,'index.html',{'news' : newsposts})
